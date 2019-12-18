ac5281e5df32de23d7af74438b578d39d01c6ad1
jenhaoyang <steven05yang@gmail.com>
2019-12-18 12:46:24 +0000

    file

--- QuestionTime/QuestionTime/wsgi.py
+++ QuestionTime/QuestionTime/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuestionTime.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuestionTime.QuestionTime.settings')
 
 application = get_wsgi_application()
